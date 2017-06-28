7a4118d1c8122da561ae935782079fb884d88ef7
Pete Phillips <petephil97@gmail.com>
2017-06-28 08:05:01 -0230

    Added hover to change colour of squares. Working on button to clear grid and ask for new grid size.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,22 @@
+function clearPad() {
+   $('.container').empty();
+}
+
+function createGrid() {
+   var numberSquares = prompt('How many squares per side would you like? (max 100)');
+   var squareSize = 720/numberSquares;
+   while (typeof numberSquares !== 'number' || numberSquares > 100 || numberSquares < 1) {
+      numberSquares = prompt('Please enter a correct value:');
+      squareSize = 720/numberSquares;
+   }
+
 $(document).ready(function() {
    for (var x = 0; x < 16; x++) {
       for (var y = 0; y < 16; y++) {
          $("<div class='square'></div>").appendTo('.container');
       }
    }
+   $('.square').hover(function() {
+      $(this).addClass('square-hover');
+   });
 });
