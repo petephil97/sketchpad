48417314c2157491645c9337a010cbf5eb0189d0
Pete Phillips <petephil97@gmail.com>
2017-06-28 14:29:57 -0230

    Added a button that clears the grid and allows it to be redrawn with custom input.

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,25 @@
-function clearPad() {
-   $('.container').empty();
-}
-
-function createGrid() {
-   var numberSquares = prompt('How many squares per side would you like? (max 100)');
-   var squareSize = 720/numberSquares;
-   while (typeof numberSquares !== 'number' || numberSquares > 100 || numberSquares < 1) {
-      numberSquares = prompt('Please enter a correct value:');
-      squareSize = 720/numberSquares;
-   }
-
 $(document).ready(function() {
+      $('.clear').on('click', function(){
+         $('.container').empty();
+         var numberSquares = prompt('How many squares per side would you like? (max 100)');
+         var squareSize = 720/numberSquares;
+         while (numberSquares > 100 || numberSquares < 1) {
+            numberSquares = prompt('Please enter a correct value:');
+            squareSize = 720/numberSquares;
+         }
+         for (var x = 0; x < numberSquares; x++) {
+            for (var y = 0; y < numberSquares; y++) {
+               $("<div class='square'></div>").appendTo('.container');
+            }
+         }
+         $('.square').css({'width': squareSize, 'height': squareSize});
+      });
    for (var x = 0; x < 16; x++) {
       for (var y = 0; y < 16; y++) {
          $("<div class='square'></div>").appendTo('.container');
       }
    }
-   $('.square').hover(function() {
+   $('.container').on('mouseenter', '.square', function() {
       $(this).addClass('square-hover');
    });
 });
