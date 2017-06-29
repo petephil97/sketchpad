c2929e405e48434855630e2074538d5f3819108e
Pete Phillips <petephil97@gmail.com>
2017-06-29 07:58:41 -0230

    Used functions to clean up JS code. Changed draw colour to red. Added title. Made cancel button functional.

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,34 @@
-$(document).ready(function() {
-      $('.clear').on('click', function(){
-         $('.container').empty();
-         var numberSquares = prompt('How many squares per side would you like? (max 100)');
-         var squareSize = 720/numberSquares;
-         while (numberSquares > 100 || numberSquares < 1) {
-            numberSquares = prompt('Please enter a correct value:');
-            squareSize = 720/numberSquares;
-         }
-         for (var x = 0; x < numberSquares; x++) {
-            for (var y = 0; y < numberSquares; y++) {
-               $("<div class='square'></div>").appendTo('.container');
-            }
-         }
-         $('.square').css({'width': squareSize, 'height': squareSize});
-      });
-   for (var x = 0; x < 16; x++) {
-      for (var y = 0; y < 16; y++) {
+var numberSquares = 16;
+var squareSize = 45;
+
+function makeGrid() {
+   for (var x = 0; x < numberSquares; x++) {
+      for (var y = 0; y < numberSquares; y++) {
          $("<div class='square'></div>").appendTo('.container');
       }
    }
+   $('.square').css({'width': squareSize, 'height': squareSize});
+}
+
+function clearPad() {
+   numberSquares = prompt('How many squares per side would you like? (max 100)');
+   if (numberSquares === null) {
+      return;
+   }
+   while (numberSquares > 100 || numberSquares < 1 || isNaN(numberSquares)) {
+      numberSquares = prompt('Please enter a correct value (must be a number, max 100)');
+      if (numberSquares === null) {
+         return;
+      }
+   }
+   $('.container').empty();
+   squareSize = 720/numberSquares;
+   makeGrid();
+}
+
+$(document).ready(function() {
+   makeGrid();
+   $('.clear').on('click', clearPad);
    $('.container').on('mouseenter', '.square', function() {
       $(this).addClass('square-hover');
    });
